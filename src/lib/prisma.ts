43638a60ceba08f7e1b34e97007ad0b979eab6c2
leoMedi09 <leomedinaflores09@gmail.com>
2026-03-03 23:34:35 -0500

    correciones

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+const libsql = createClient({
+    url: process.env.TURSO_DATABASE_URL || "",
+    authToken: process.env.TURSO_AUTH_TOKEN,
+})
+
+const adapter = new PrismaLibSQL(libsql)
 
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient()
+    globalForPrisma.prisma ?? new PrismaClient({ adapter } as any)
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
